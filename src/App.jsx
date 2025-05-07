0921a1196cd5a3effaad39228997263d6861136a
MTruong2000 <truongdev2000@gmail.com>
2025-05-07 10:58:01 +0700

    fix/add logic add box

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import { useEffect } from 'react';
 import '@n8n/chat/style.css';
 import { createChat } from '@n8n/chat';
-
 import './App.css';
 
 function App() {
     useEffect(() => {
-        createChat({
+        const chatBox = createChat({
             webhookUrl: 'https://nightfurys.app.n8n.cloud/webhook/65afb57e-1c89-44a3-b9bb-84013a4511d7/chat',
         });
+
+        chatBox.on('open', () => {
+            window.parent.postMessage({ type: 'CHATBOX_OPEN' }, '*');
+        });
+
+        chatBox.on('close', () => {
+            window.parent.postMessage({ type: 'CHATBOX_CLOSED' }, '*');
+        });
+
+        return () => {
+            chatBox.off('open');
+            chatBox.off('close');
+        };
     }, []);
 
-    return <></>;
+    return null;
 }
 
 export default App;
